4c014023df0a5a5efad7398de3f865e96c3289ac
Hubert Pompecki <hpompecki@gmail.com>
2017-05-05 16:03:14 +0100

    Fix component.js template

--- templates/component.js
+++ templates/component.js
@@ -1,6 +1,6 @@
 // @flow
 
 // AWS Lambda will call this method
-exports.process = async (event: {}, context: {}, callback: (Error | null, {} | void) => void) => {
+exports.process = async (event: {}, context: {}, callback: (Error | null, any) => void) => {
   callback(null, 'Hello, World!')
 }
