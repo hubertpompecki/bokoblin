5640354b74ef17732f670f58d67e2b22005ed984
Hubert Pompecki <hpompecki@gmail.com>
2017-05-05 15:56:28 +0100

    bugfix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const program = require('commander')
 const fs = require('fs')
-const fs = require('path')
+const path = require('path')
 const execSync = require('child_process').execSync;
 const commandExistsSync = require('command-exists').sync;
 
